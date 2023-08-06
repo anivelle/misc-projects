a576dc070f6679de4b17120149b3de344aba83b8
Alex Nivelle <anivelle@ucsc.edu>
2023-08-05 22:10:45 -0700

    Note to self. Breaking the Arduino by erasing a non word-aligned page is a bad idea. Great start to this repo

--- write-flash/write-flash.ino
+++ write-flash/write-flash.ino
@@ -1,30 +1,40 @@
 #include "mbed.h"
 #include <cstdlib>
 
-int *testWrite = reinterpret_cast<int *>(0x123);
+#define WRITEVAL 13
+
+// Saw this as a method to create a pointer with a specific address
+// https://stackoverflow.com/questions/3934775/how-to-initialize-a-pointer-to-a-specific-memory-address-in-c
+uint32_t *testWrite = reinterpret_cast<uint32_t *>(0x1000);
 
 void setup() {
     Serial.begin(115200);
     while (!Serial) {
     };
-    NRF_NVMC->CONFIG = 1;
+    Serial.println((uint32_t)testWrite);
+    Serial.println(*testWrite);
+    NRF_NVMC->CONFIG = 2;
+    if (*testWrite != 0xFFFFFFFF && *testWrite != WRITEVAL)
+        // If page was not empty earlier
+        NRF_NVMC->ERASEPAGE = (uint32_t)testWrite;
+    else
+        Serial.println("Page is still not empty");
 
-    while (!NRF_NVMC->READY) {
-    };
     // The first time the device gets turned on, this should write to flash and
     // then just spin lock. Defaults to 0xFFFFFFFF because the NVM is
-    // initialized to all 1s
+    // initialized to all 1s. I think.
     if (*testWrite == 0xFFFFFFFF) {
-        *testWrite = 13;
-        while (true) {
+        *testWrite = WRITEVAL;
+        // Just waits until we are done with the write to continue
+        while (!NRF_NVMC->READY) {
         };
+        NRF_NVMC->CONFIG = 0;
     }
-    NRF_NVMC->CONFIG = 0;
 }
 
 void loop() {
     // The conditional is probably unnecessary but I felt like using it
-    if (!*testWrite) {
+    if (*testWrite != 0xFFFFFFFF) {
         Serial.println(*testWrite);
     }
 }
