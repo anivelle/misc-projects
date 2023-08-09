1d54c4d909c339e2e2fcfa2f4b792c28920bc3f9
Alex Nivelle <anivelle@ucsc.edu>
2023-08-08 17:38:58 -0700

    Changed up the playback method, but for some reason closing the stream breaks now

--- FFT/FFT.c
+++ FFT/FFT.c
@@ -7,12 +7,16 @@
 #include <raylib.h>
 
 #define LENGTH 200
+
+// 1024 is consistently the amount of frames available to write
+#define FRAMECOUNT 1024
 #define SEQ_STEP 0.1
 int callback(const void *input, void *output, unsigned long frameCount,
              const PaStreamCallbackTimeInfo *timeInfo,
              PaStreamCallbackFlags statusFlags, void *userData);
 
 int ended = 0;
+char *file = "libera.mp3";
 
 int main(int argc, char *argv[]) {
     PaErrorCode err = Pa_Initialize();
@@ -20,18 +24,27 @@ int main(int argc, char *argv[]) {
         printf("Issue starting PortAudio.\n");
         return 1;
     }
-    const PaHostApiInfo *host = Pa_GetHostApiInfo(Pa_GetDefaultHostApi());
-    const PaDeviceInfo *indev = Pa_GetDeviceInfo(host->defaultInputDevice);
-    const PaDeviceInfo *outdev = Pa_GetDeviceInfo(host->defaultOutputDevice);
 
     SF_INFO info;
     info.format = 0;
 
-    SNDFILE *music = sf_open("libera.mp3", SFM_READ, &info);
+    // Take command-line input to select a file for now
+    if (argc > 1)
+        file = argv[1];
+
+    // Open audio file for reading
+    SNDFILE *music = sf_open(file, SFM_READ, &info);
     PaStream *stream;
 
+    // Synchronous playback - will use this so I have easier access to the data
+    // that is being read.
     err = Pa_OpenDefaultStream(&stream, 0, info.channels, paFloat32,
-                               info.samplerate, 512, &callback, music);
+                               info.samplerate, 512, NULL, music);
+
+    // Asynchronous callback - cool but I'm not sure how the timing will work
+    // when trying to capture a specific frame of data to read
+    /* err = Pa_OpenDefaultStream(&stream, 0, info.channels, paFloat32, */
+    /*                            info.samplerate, 512, &callback, music); */
 
     if (err != paNoError) {
         printf("Error opening stream\n");
@@ -39,13 +52,26 @@ int main(int argc, char *argv[]) {
     }
 
     err = Pa_StartStream(stream);
+    sf_count_t count = -1;
+    signed long available;
 
-    // Wait for the song to end
-    while (!ended) {
+    float *frames = malloc(FRAMECOUNT * info.channels);
+
+    while (true) {
+        available = Pa_GetStreamWriteAvailable(stream);
+        if (available) {
+            /* printf("%ld\n", available); */
+            count = sf_readf_float(music, frames, available);
+            /* printf("%ld\n", count); */
+            Pa_WriteStream(stream, frames, available);
+            if (count < available)
+                break;
+        }
     };
-    err = Pa_StopStream(stream);
+    // err = Pa_StopStream(stream);
 
     // Just for funsies
+
     float sequence[LENGTH];
     float complex output[LENGTH];
     for (int i = 0; i < LENGTH; i++) {
@@ -60,25 +86,28 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int callback(const void *input, void *output, unsigned long frameCount,
-             const PaStreamCallbackTimeInfo *timeInfo,
-             PaStreamCallbackFlags statusFlags, void *userData) {
-    SNDFILE *music = (SNDFILE *)userData; // Take in the audio file
-    float *out = (float *)output;         // Cast output buffer
-    (void)input;
+// No longer in use
+/* int callback(const void *input, void *output, unsigned long frameCount, */
+/*              const PaStreamCallbackTimeInfo *timeInfo, */
+/*              PaStreamCallbackFlags statusFlags, void *userData) { */
+/*     SNDFILE *music = (SNDFILE *)userData; // Take in the audio file */
+/*     float *out = (float *)output;         // Cast output buffer */
+/*     (void)input; */
 
-    // https://github.com/hosackm/wavplayer/blob/master/src/wavplay.c
-    // Realized I had to multiply the frameCount by the number of channels in
-    // the audio from this page. Also helped with stream starting
-    //
-    // Reads audio from the input file straight into the output buffer
-    sf_count_t count = sf_read_float(music, out, frameCount * 2);
+/*     // https://github.com/hosackm/wavplayer/blob/master/src/wavplay.c */
+/*     // Realized I had to multiply the frameCount by the number of channels in
+ */
+/*     // the audio from this page. Also helped with stream starting */
+/*     // */
+/*     // Reads audio from the input file straight into the output buffer */
+/*     sf_count_t count = sf_read_float(music, out, frameCount * 2); */
 
-    // Same way file reading works. If we read less than expected, we've reached
-    // the end
-    if (count == frameCount * 2)
-        return paContinue;
+/*     // Same way file reading works. If we read less than expected, we've
+ * reached */
+/*     // the end */
+/*     if (count == frameCount * 2) */
+/*         return paContinue; */
 
-    ended = 1; // Mark the ending of the song
-    return paComplete;
-}
+/*     ended = 1; // Mark the ending of the song */
+/*     return paComplete; */
+/* } */
