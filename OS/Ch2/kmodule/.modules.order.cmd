9f15398571037106a5f328aea5c7c29e44fefb32
Alex Nivelle <anivelle@ucsc.edu>
2023-12-05 17:13:43 -0800

    Removed OS-related files since they were moved to a different repository

--- OS/Ch2/kmodule/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/alex/Projects/OS/Ch2/kmodule/modules.order := {   echo /home/alex/Projects/OS/Ch2/kmodule/simple.o; :; } > /home/alex/Projects/OS/Ch2/kmodule/modules.order
--- OS/Ch2/kmodule/proc/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/alex/Projects/OS/Ch2/kmodule/proc/modules.order := {   echo /home/alex/Projects/OS/Ch2/kmodule/proc/proc.o; :; } > /home/alex/Projects/OS/Ch2/kmodule/proc/modules.order
